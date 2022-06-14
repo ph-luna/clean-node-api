8adc51cfade7e056ed5b48ee5cbe0b81af0bda7f
ph-luna <ph.luna.vieira@gmail.com>
2022-06-14 13:07:27 -0300

    test: ensure SignUpControler return status 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'fake.email@email.com',
+        password: 'fake_password123',
+        passwordConfirmation: 'fake_password123'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
